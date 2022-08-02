5c84a30a14f7a447f6314761cfe876efdeb2cc8f
soma5722 <89482153+soma5722@users.noreply.github.com>
2022-08-01 20:13:23 -0700

    Data Analysis

---
+++ DataAnalysis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Data Sets/movies.csv')
+#print(df.nunique(axis=0))
+#print(df['movieId'].nunique())
+movieId = df['movieId'].nunique()
+genre = df['genres'].nunique()
+
+df = pd.read_csv('Data Sets/ratings.csv')
+#print(df.nunique(axis=0))
+userId = df['userId'].nunique()
+ratings = df['rating'].nunique()
+
+df = pd.read_csv('Data Sets/tags.csv')
+#print(df.nunique(axis=0))
+tag = df['tag'].nunique()
+x=['movieId','genres','userId','rating','tag']
+y = [movieId,genre,userId,ratings,tag]
+print(x)
+print(y)
+  
+fig = plt.figure(figsize = (10, 5))
+
+plt.barh(x, y)
+ 
+for index, value in enumerate(y):
+    plt.text(value, index,
+             str(value)) 
+# creating the bar plot
+#plt.bar(x, y, color ='maroon',width = 0.4)
+ 
+plt.ylabel("Unique attributes")
+plt.xlabel("Count")
+plt.title("Unique attributes vs Count")
+plt.show()
+
+#sns.barplot(x=['movieId','genres','userId','rating','tag'], y = [movieId,genre,userId,ratings,tag],palette="Blues_d")    
